b70ccf4d5d365450324c4cdedc8590cd8fd57878
k85246 <karim.s.s@hotmail.com>
2023-03-22 22:46:36 +0200

    modify the parameter to put the dimension only you want to modify (default is zero)

--- image_captioning/lib/slideAnimation.dart
+++ image_captioning/lib/slideAnimation.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 
 class SlideAnimation extends PageRouteBuilder{
   final page;
-  double begin_x  ;
-  double begin_y  ;
-  double end_x  ;
-  double end_y  ;
+  double beginX;
+  double beginY;
+  double endX;
+  double endY;
 
-  SlideAnimation({this.page,   required this.begin_x,required this.begin_y,
-   required this.end_x,required this.end_y}) : super(
+  SlideAnimation({this.page, this.beginX = 0, this.beginY = 0,
+    this.endX = 0, this.endY = 0}) : super(
     pageBuilder: (context,animation,animationtwo) => page,
     transitionsBuilder: (context,animation,animationtwo,child) {
-      var begin = Offset(begin_x , begin_y);
-      var end = Offset(end_x,end_y);
-      var curve = Curves.ease;
+      Offset begin = Offset(beginX,beginY);
+      Offset end = Offset(endX,endY);
+      Curve curve = Curves.ease;
       var tween = Tween(begin:begin ,end:end ).chain(CurveTween(curve: curve));
       var offsetAnimation = animation.drive(tween);
       return SlideTransition(position: offsetAnimation,
@@ -21,6 +21,5 @@ class SlideAnimation extends PageRouteBuilder{
       );
     }
   );
-
 }
 
